20da8af8d184db88c7a8de89a3add0c17048eb6f
qm210 <quantenmace@gmail.com>
2024-06-11 15:27:21 +0200

    init commit

---
+++ gmae/__main__.py
@@ -0,0 +1,3 @@
+import cv2
+print("CV2 version", cv2.__version__)
+
---
+++ test_picamera.py
@@ -0,0 +1,28 @@
+import picamera2
+import cv2
+import numpy as np
+
+print("OpenCV version", cv2.__version__)
+
+"""
+Does not work real-time, but records a file with picamera2 and displays that with cv2
+"""
+
+with picamera2.PiCamera() as camera:
+    camera.start_preview()
+    camera.resolution = (1920, 1080)
+    camera.start_recording('output.h264', format='h264')
+    time.sleep(10)
+    camera.stop_recording()
+
+cap = cv2.VideoCapture('output.h264')
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
